6b155d906607cae2a86512943ceb5885b2cee052
Kay-Nicté <isaea19@gmail.com>
2024-10-30 01:56:07 +0100

    Añadir tabla que suma total de destinos teniendo en cuenta las personas

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -17,41 +17,69 @@ const CartPage: React.FC<CartPageProps> = ({ reservedDestinations, onRemoveFromC
             [destinationId]: count,
         }));
     };
+
+    // Calcular el precio total por todos los destinos
+    const calculateTotalPrice = () => {
+        return reservedDestinations.reduce((total, destination) => {
+            const personCount = personCounts[destination.id] || 1; // Número de personas por destino
+            return total + (destination.price * personCount); // Sumar el precio por el número de personas
+        }, 0);
+    };
+
     return (
-        <div>
-            <h2>Carrito</h2>
+        <div className="container">
+            <h2 className='text-center my-4'>Carrito</h2>
             {reservedDestinations.length > 0 ? (
-                <ul>
-                    {reservedDestinations.map((destination: Destination) => (
-                        <li key={destination.id}>
-                            <div className="d-flex flex-row align-items-center">
-                                <span className="me-2">{destination.name}</span>
-
-                                <select
-                                    className="form-select me-2"
-                                    value={personCounts[destination.id] || 1} // Valor por defecto 1
-                                    onChange={(e) => handlePersonCountChange(destination.id, Number(e.target.value))}
-                                    style={{ width: '130px' }}
-                                >
-                                    {[1, 2, 3, 4, 5].map((count) => (
-                                        <option key={count} value={count}>
-                                            {count} {count > 1 ? 'personas' : 'persona'}
-                                        </option>
-                                    ))}
-                                </select>
-
-                                <button
-                                    style={{ background: 'none', border: 'none', color: 'red', cursor: 'pointer' }}
-                                    onClick={() => onRemoveFromCart(destination)}
-                                    aria-label="Eliminar destino"
-                                >
-                                    <FaTimes />
-                                </button>
-                            </div>
-
-                        </li>
-                    ))}
-                </ul>
+                <div className="table-responsive text-center">
+                    <table className="table table-striped table-bordered table-dark">
+                        <thead>
+                            <tr>
+                                <th className='w-25'>Destino</th>
+                                <th className='w-25'>Personas</th>
+                                <th className='w-25'>Precio</th>
+                                <th className='w-25'>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {reservedDestinations.map((destination: Destination) => (
+                                <tr key={destination.id}>
+                                    <td>{destination.name}</td>
+                                    <td>
+                                        <select
+                                            className="form-select text-center"
+                                            value={personCounts[destination.id] || 1} // Valor por defecto 1
+                                            onChange={(e) => handlePersonCountChange(destination.id, Number(e.target.value))}
+                                        >
+                                            {[1, 2, 3, 4, 5].map((count) => (
+                                                <option key={count} value={count}>
+                                                    {count} {count > 1 ? 'personas' : 'persona'}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </td>
+                                    <td>
+                                        {(destination.price * (personCounts[destination.id] || 1))}€
+                                    </td>
+                                    <td>
+                                        <button
+                                            style={{ background: 'none', border: 'none', color: 'red', cursor: 'pointer' }}
+                                            onClick={() => onRemoveFromCart(destination)}
+                                            aria-label="Eliminar destino"
+                                        >
+                                            <FaTimes />
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colSpan={2} className="text-end fs-5"><strong>Total</strong></td>
+                                <td colSpan={2} className='text-start fs-5'><strong>{calculateTotalPrice()}€</strong></td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
             ) : (
                 <p>No hay destinos reservados.</p>
             )}
